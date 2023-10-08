8f6b93891ad4d27361dcfaa9a5821b924fbb79f2
Oleg <110499237+radical-subject@users.noreply.github.com>
2023-10-08 19:50:14 +0300

    tests

--- INPUT_PREPARATION/DrOligoInputPreparation_withFormsAPI.py
+++ INPUT_PREPARATION/DrOligoInputPreparation_withFormsAPI.py
@@ -17,8 +17,7 @@ from warnings import catch_warnings
 
 # MayaChemTools imports...
 try:
-    import MiscUtil
-    from docopt import docopt
+    from docopt import MiscUtil, docopt
 except ImportError as ErrMsg:
     sys.stderr.write("\nFailed to import MayaChemTools module/package: %s\n" % ErrMsg)
     sys.stderr.write("Check/update your MayaChemTools environment and try again.\n\n")
@@ -40,11 +39,19 @@ from oauth2client import client, file, tools
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--name", required=True)
 args = parser.parse_args()
-print(f"Hi {args.name} , Welcome ")
+EXPERIMENT_NAME = f"{args.name}"
 
 ScriptName = os.path.basename(sys.argv[0])
 Options = {}
 OptionsInfo = {}
+
+#################
+## CREATE DIR  ##
+#################
+cwd = os.getcwd()
+dir = os.path.join(cwd, "requests", EXPERIMENT_NAME)
+if not os.path.exists(dir):
+    os.mkdir(dir)
 
 #################
 ## GOOGLE AUTH ##
@@ -53,17 +60,16 @@ SCOPES = "https://www.googleapis.com/auth/drive"
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
 
 
-if os.path.exists("C:\\Users\\fedorov_ov\\Desktop\\token.json"):
-    creds = Credentials.from_authorized_user_file(
-        "C:\\Users\\fedorov_ov\\Desktop\\token.json", SCOPES
-    )
-else:
-    store = file.Storage("token.json")
-    creds = None
-    if not creds:
-        flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
-        creds = tools.run_flow(flow, store)
-
+# if os.path.exists("C:\\Users\\fedorov_ov\\Desktop\\token.json"):
+#     creds = Credentials.from_authorized_user_file(
+#         "C:\\Users\\fedorov_ov\\Desktop\\token.json", SCOPES
+#     )
+# else:
+store = file.Storage("token.json")
+creds = None
+if not creds:
+    flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
+    creds = tools.run_flow(flow, store)
 
 ##################
 ## GOOGLE FORMS ##
@@ -118,62 +124,62 @@ for file in file_list:
     download_request_file(file[0]["fileId"], file[0]["fileName"])
 
 
-def RetrieveOptions():
-    """Retrieve command line arguments and options."""
+# def RetrieveOptions():
+#     """Retrieve command line arguments and options."""
 
-    # Get options...
-    global Options
-    Options = docopt(_docoptUsage_)
+#     # Get options...
+#     global Options
+#     Options = docopt(_docoptUsage_)
 
-    # Set current working directory to the specified directory...
-    WorkingDir = Options["--workingdir"]
-    if WorkingDir:
-        os.chdir(WorkingDir)
+#     # Set current working directory to the specified directory...
+#     WorkingDir = Options["--workingdir"]
+#     if WorkingDir:
+#         os.chdir(WorkingDir)
 
-    # Handle examples option...
-    if "--examples" in Options and Options["--examples"]:
-        MiscUtil.PrintInfo(MiscUtil.GetExamplesTextFromDocOptText(_docoptUsage_))
-        sys.exit(0)
-
-
-# Setup a usage string for docopt...
-_docoptUsage_ = """
-DrOligoInputPreparation_withFormsAPI.py - DrOligo Input preparation
-
-Usage:
-    DrOligoInputPreparation_withFormsAPI.py [-n <exp name>] 
-    DrOligoInputPreparation_withFormsAPI.py -h | --help | -e | --examples
-
-Description:
-    Generate input files for DrOligo768XLC from requests submitted in google forms:
-
-    The supported input file formats are:  .csv, .tsv, .txt, xslx
-
-    The supported output file format are: SD (.csv, .xlsx)
-
-Options:
-    -n, --name 
-    -e, --examples
-        Print examples.
-    -h, --help
-        Print this help message.
+#     # Handle examples option...
+#     if "--examples" in Options and Options["--examples"]:
+#         MiscUtil.PrintInfo(MiscUtil.GetExamplesTextFromDocOptText(_docoptUsage_))
+#         sys.exit(0)
 
 
-Examples:
-    To download files with oligos for synthesis from google forms and prepare inputs for Dr.Oligo run:
-        % DrOligoInputPreparation_withFormsAPI.py -n KJE0006
+# # Setup a usage string for docopt...
+# _docoptUsage_ = """
+# DrOligoInputPreparation_withFormsAPI.py - DrOligo Input preparation
+
+# Usage:
+#     DrOligoInputPreparation_withFormsAPI.py [-n <exp name>] 
+#     DrOligoInputPreparation_withFormsAPI.py -h | --help | -e | --examples
+
+# Description:
+#     Generate input files for DrOligo768XLC from requests submitted in google forms:
+
+#     The supported input file formats are:  .csv, .tsv, .txt, xslx
+
+#     The supported output file format are: SD (.csv, .xlsx)
+
+# Options:
+#     -n, --name 
+#     -e, --examples
+#         Print examples.
+#     -h, --help
+#         Print this help message.
+
+
+# Examples:
+#     To download files with oligos for synthesis from google forms and prepare inputs for Dr.Oligo run:
+#         % DrOligoInputPreparation_withFormsAPI.py -n KJE0006
         
-        or (optional):
+#         or (optional):
         
-        % DrOligoInputPreparation_withFormsAPI.py -n KJE0006 --format csv
+#         % DrOligoInputPreparation_withFormsAPI.py -n KJE0006 --format csv
 
-Author:
-    OLEG FEDOROV (o.fedorov@sysbiomed.ru)
+# Author:
+#     OLEG FEDOROV (o.fedorov@sysbiomed.ru)
 
-Copyright:
-    Copyright (C) 2023 OLEG FEDOROV. All rights reserved.
+# Copyright:
+#     Copyright (C) 2023 OLEG FEDOROV. All rights reserved.
 
-"""
+# """
 
 
 ##########

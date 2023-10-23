835cb923ec2a4b1066b7df0760130ba7883256e2
Oleg <110499237+radical-subject@users.noreply.github.com>
2023-10-23 23:35:44 +0300

    summary

--- FASTAPI_FOLDER/main.py
+++ FASTAPI_FOLDER/main.py
@@ -1,59 +1,60 @@
-# from typing import Union
-
-# from fastapi import FastAPI
-# from modules.input_cleaner import combined_pipeline
-# from modules.send_email import send_email
-# from modules.tg_notif import check_and_notify
-# from pydantic import BaseModel
-
-# app = FastAPI()
-
-
-# class Item(BaseModel):
-#     name: str
-
-
-# class Item_for_tg_notif(BaseModel):
-#     name: str
-#     chat_id: str
-
-
-# @app.post("/send_email_with_data/")
-# async def create_item(item: Item):
-#     """
-#     передаем EXPERIMENT_NAME переменную текстом. пример: `KJE0008`
-#     """
-#     send_email(item.name)
-#     return item
-
-
-# @app.post("/prepare_inputs/")
-# async def create_item(item: Item):
-#     """
-#     передаем EXPERIMENT_NAME переменную текстом. пример: `KJE0008`
-#     """
-#     combined_pipeline(item.name)
-#     return item
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
-# @app.post("/send_notif_tg/")
-# async def create_item_id(item: Item_for_tg_notif):
-#     """
-#     передаем chat_id переменную текстом. пример: `234234235`
-#     """
-#     check_and_notify(item.name, item.chat_id)
-#     return item
+from typing import Union
 
 from fastapi import FastAPI
+from modules.input_cleaner import combined_pipeline
+from modules.send_email import send_email
+from modules.tg_notif import check_and_notify
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    name: str
+
+
+class Item_for_tg_notif(BaseModel):
+    name: str
+    chat_id: str
+
+
+@app.post("/send_email_with_data/")
+async def create_item(item: Item):
+    """
+    передаем EXPERIMENT_NAME переменную текстом. пример: `KJE0008`
+    """
+    send_email(item.name)
+    return item
+
+
+@app.post("/prepare_inputs/")
+async def create_item(item: Item):
+    """
+    передаем EXPERIMENT_NAME переменную текстом. пример: `KJE0008`
+    """
+    combined_pipeline(item.name)
+    return item
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/send_notif_tg/")
+async def create_item_id(item: Item_for_tg_notif):
+    """
+    передаем chat_id переменную текстом. пример: `234234235`
+    """
+    check_and_notify(item.name, item.chat_id)
+    return item
+
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}

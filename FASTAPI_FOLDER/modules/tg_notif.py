1a9cf033cf496af47455a9a5aee82ccef5a59b53
Oleg <110499237+radical-subject@users.noreply.github.com>
2023-10-23 23:54:43 +0300

    Update tg_notif.py

--- FASTAPI_FOLDER/modules/tg_notif.py
+++ FASTAPI_FOLDER/modules/tg_notif.py
@@ -1,43 +1,41 @@
+import os
+import time
 
-import time, os
 import requests
+
 # chat_id = "403278079"
+
 
 def check_and_notify(name, chat_id):
     # Замените следующие значения на ваш Telegram bot token и chat_id
     bot_token = "6413423789:AAElRZhIcR-iQaY_6jsUx3Mag-MtyIpCQzw"
     notification_sent = False
-    
-    filepath = os.path.join('/home/fedorov_ov/folders/RUNFILES', f"{name}.txt")
+
+    filepath = os.path.join("/RUNFILES", f"{name}.txt")
     while True:
-        
-        with open(filepath, 'r') as f:
+        with open(filepath, "r") as f:
             content = f.read()
-            if 'Synthesis complete' in content and not notification_sent:
+            if "Synthesis complete" in content and not notification_sent:
                 url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-                payload = {
-                    "chat_id": chat_id,
-                    "text": 'Synthesis complete'
-                }
+                payload = {"chat_id": chat_id, "text": "Synthesis complete"}
                 requests.post(url, payload)
-                print('Telegram notification sent.')
-                notification_sent = True  # Установите флаг, чтобы не отправлять уведомление снова
+                print("Telegram notification sent.")
+                notification_sent = (
+                    True  # Установите флаг, чтобы не отправлять уведомление снова
+                )
                 break  # Выход из цикла
             else:
-                print('Synthesis not complete yet. Checking again in 2 minutes.')
-                
+                print("Synthesis not complete yet. Checking again in 2 minutes.")
+
                 url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
                 payload = {
                     "chat_id": chat_id,
-                    "text": 'Synthesis not complete yet. Checking again in 2 minutes.'
+                    "text": "Synthesis not complete yet. Checking again in 2 minutes.",
                 }
                 requests.post(url, payload)
-                print('Telegram notification sent.')
-                notification_sent = False  # Установите флаг, чтобы не отправлять уведомление снова
-                
+                print("Telegram notification sent.")
+                notification_sent = (
+                    False  # Установите флаг, чтобы не отправлять уведомление снова
+                )
+
                 time.sleep(120)  # Пауза на 2 минуты
-
-
-
-
-
